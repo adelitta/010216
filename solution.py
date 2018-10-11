21d72ec03e8813f989e3122078c83dccded3b0e1
adelitta <44044970+adelitta@users.noreply.github.com>
2018-10-11 12:52:20 -0400

    Create solution.py

---
+++ solution.py
@@ -0,0 +1,13 @@
+def piglatin(sentence):
+	sentence = sentence.split()
+	vowels = "aeiouy"
+	for i in range(len(sentence)):
+		if sentence[i][0] in vowels:
+			sentence[i] += 'yay'
+		else:
+			while 
+				
+				sentence[w]=sentence[i][1:]+sentence[i][0]
+			sentence[i]+='ay'
+		sentence = ' '.join(sentence)
+		return sentence
